4fda589d7167b6d99e9f1555b3e8309968989173
felipeha323 <60258356+felipeha323@users.noreply.github.com>
2020-01-31 07:11:41 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            string[] bingo = new string[] { "B  ", " I  ", " N  ", " G  ", " O  " };
+            int[,] NumAle = new int[5, 5];
+            int min = 1, max = 15;
+            var random = new Random();
+            string tachar;
+            Console.WriteLine("************************************************************************************************************************");
+            Console.WriteLine(bingo[0] + bingo[1] + bingo[2] + bingo[3] + bingo[4]);
+            Console.WriteLine("\n");
+
+            for (int i=0; i < 5; i++){
+
+                for(int j = 0; j < 5; j++){
+
+                    if (i == 2 && j == 2)
+                    {
+                        NumAle[i, j] = 0;
+                    }
+                    else
+                    {                      
+                        NumAle[j, i] = random.Next(min, max);
+                    }
+                                    }
+                min += 15;
+                max += 15;
+                
+            }
+            
+            Tablero bingo1 = new Tablero(bingo, NumAle);
+            
+            Console.WriteLine(bingo1.ImprimirM());
+
+            while (1 == 1)
+            {
+                Console.WriteLine("Ingrese la posicion ganadora: ");
+                tachar = Console.ReadLine();
+                bingo1.Tachar(tachar);
+                Console.WriteLine("\n");
+                Console.WriteLine("************************************************************************************************************************");
+                Console.WriteLine(bingo[0] + bingo[1] + bingo[2] + bingo[3] + bingo[4]);
+                Console.WriteLine("\n");
+                Console.WriteLine(bingo1.ImprimirM());
+            }
+
+            /*
+            int F = 0;
+                int C = 0;
+                int[,] MAT = new int[6, 6];
+                Random rnd = new Random();
+                // GENERO LOS NÚMEROS
+                for (F = 1; F <= 5; F++)
+                {
+                    for (C = 1; C <= 5; C++)
+                    {
+                        MAT[F, C] = rnd.Next(0, 99);
+                    }
+                }
+                // SALIDA
+                for (F = 1; F <= 5; F++)
+                {
+                    for (C = 1; C <= 5; C++)
+                    {
+                        // APROVECHAMOS CICLOS Y GENERAMOS POSICIÓN EN PANTALLA
+                        // PARA COLUMNA Y FILA
+                        Console.SetCursorPosition(C * 4, F + 1);
+                        Console.Write(MAT[F, C]);
+                    }
+                    
+              
+                }
+                Console.WriteLine();
+                Console.Write("Pulse una Tecla:");
+                Console.ReadLine();
+            */
+
+
+
+        }
+    }
+}
---
+++ Tablero.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    class Tablero
+    {
+        private string[] bingo1;
+        private int[,] NumAle;
+
+        public Tablero(string[] bingo, int[,] numAle)
+        {
+            bingo1 = bingo;
+            NumAle = numAle;
+        }
+
+        public string[] Bingo1 { get => bingo1; set => bingo1 = value; }
+        public int[,] NumAle1 { get => NumAle; set => NumAle = value; }
+
+        public int[,] Tachar(string pos)
+        {
+            string []split = (pos.Split(','));
+            
+            for(int i = 0; i <5; i++)
+            {
+                for(int j = 0; j < 5; j++)
+                {
+                    if(i== Convert.ToInt32(split[0]) && j == Convert.ToInt32(split[1]))
+                    {
+                        NumAle[i, j] = 0;
+                    }
+                }
+            }
+            return NumAle;
+        }
+
+        public string ImprimirM()
+        {
+            string nMatriz = "";
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    nMatriz += NumAle[i, j] + " || ";
+                }
+                nMatriz += "\n";
+            }
+            return nMatriz;
+        }
+    }
+}
